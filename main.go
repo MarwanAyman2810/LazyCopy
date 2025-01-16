b55ecf3d0e828e31ac84c1f285eaa611be5f4dba
MarwanAyman2810 <113639297+MarwanAyman2810@users.noreply.github.com>
2025-01-16 23:07:14 +0200

    added paste&go button for search bars

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/atotto/clipboard"
 	"github.com/go-vgo/robotgo"
@@ -30,12 +31,13 @@ func main() {
 
 	// Create the copy window
 	copyWindow := myApp.NewWindow("Copy/Paste")
-	copyWindow.Resize(fyne.NewSize(300, 150))
+	copyWindow.Resize(fyne.NewSize(50, 50))
 	copyWindow.SetFixedSize(true)
 	copyWindow.Canvas().Focused()
 	copyWindow.Hide()
 
 	var selectedText string
+	var lastSelection string
 
 	// Add label to display selected text
 	textLabel := widget.NewLabel("")
@@ -47,6 +49,7 @@ func main() {
 			// Window stays open and on top
 		}
 	})
+	copyBtn.Resize(fyne.NewSize(100, copyBtn.MinSize().Height))
 
 	// Add paste button
 	pasteBtn := widget.NewButton("Paste", func() {
@@ -55,29 +58,45 @@ func main() {
 		time.Sleep(100 * time.Millisecond)
 		robotgo.KeyTap("v", "ctrl")
 	})
+	pasteBtn.Resize(fyne.NewSize(100, pasteBtn.MinSize().Height))
 
-	// Use vertical box to stack label and buttons
+	// Add paste and go button
+	pasteGoBtn := widget.NewButton("Paste & Go", func() {
+		copyWindow.Hide()
+		// Give a small delay for window focus to change
+		time.Sleep(100 * time.Millisecond)
+		robotgo.KeyTap("v", "ctrl")
+		time.Sleep(50 * time.Millisecond)
+		robotgo.KeyTap("enter")
+	})
+	pasteGoBtn.Resize(fyne.NewSize(100, pasteGoBtn.MinSize().Height))
+
+	// Create a 1x3 grid layout for buttons (1 row, 3 columns)
+	buttonsBox := container.New(layout.NewGridLayout(3), copyBtn, pasteBtn, pasteGoBtn)
+
 	content := container.NewVBox(
 		textLabel,
-		copyBtn,
-		pasteBtn,
+		buttonsBox,
 	)
 	copyWindow.SetContent(container.NewPadded(content))
 
 	// Monitor text selection
 	go func() {
+		// Initial state
+		lastSelection = getSelectedText()
 		isWindowVisible := false
+
 		for {
 			newSelection := getSelectedText()
-			if newSelection != "" && newSelection != selectedText {
+			// Only show window if there's a new selection different from the last one
+			if newSelection != "" && newSelection != lastSelection {
 				selectedText = newSelection
+				lastSelection = newSelection
 				textLabel.SetText(selectedText)
-				if !isWindowVisible {
-					copyWindow.Show()
-					copyWindow.CenterOnScreen()
-					copyWindow.RequestFocus()
-					isWindowVisible = true
-				}
+				copyWindow.Show()
+				copyWindow.CenterOnScreen()
+				copyWindow.RequestFocus()
+				isWindowVisible = true
 			} else if newSelection == "" && isWindowVisible {
 				copyWindow.Hide()
 				isWindowVisible = false
