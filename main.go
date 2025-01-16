7b9f55fb2f9deb355604e30979ef2f28a5bb2a16
MarwanAyman2810 <113639297+MarwanAyman2810@users.noreply.github.com>
2025-01-16 14:47:35 +0200

    removed Copy Catcher window
    removed unnecessary code

--- main.go
+++ main.go
@@ -4,45 +4,13 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 	"github.com/atotto/clipboard"
 )
-
-// ClickCatcher implements desktop.Mouseable
-type ClickCatcher struct {
-	widget.BaseWidget
-	lastClick  time.Time
-	copyWindow fyne.Window
-}
-
-func NewClickCatcher(copyWin fyne.Window) *ClickCatcher {
-	c := &ClickCatcher{copyWindow: copyWin}
-	c.ExtendBaseWidget(c)
-	return c
-}
-
-func (c *ClickCatcher) MouseDown(*desktop.MouseEvent) {
-	fmt.Println("Mouse click detected!")
-	now := time.Now()
-	if now.Sub(c.lastClick) < 500*time.Millisecond {
-		fmt.Println("Double click detected! Showing window...")
-		c.copyWindow.CenterOnScreen()
-		c.copyWindow.Show()
-		c.copyWindow.RequestFocus()
-	}
-	c.lastClick = now
-}
-
-func (c *ClickCatcher) MouseUp(*desktop.MouseEvent) {}
-func (c *ClickCatcher) CreateRenderer() fyne.WidgetRenderer {
-	return widget.NewSimpleRenderer(container.NewPadded())
-}
 
 func getSelectedText() string {
 	cmd := exec.Command("xclip", "-o", "-selection", "primary")
@@ -55,11 +23,9 @@ func getSelectedText() string {
 }
 
 func main() {
-	fmt.Println("Starting application...")
 	myApp := app.New()
 
-	// Create only the copy button window
-	fmt.Println("Creating copy window...")
+	// Create the copy window
 	copyWindow := myApp.NewWindow("Copy")
 	copyWindow.Resize(fyne.NewSize(100, 40))
 	copyWindow.SetFixedSize(true)
@@ -76,13 +42,7 @@ func main() {
 	})
 	copyWindow.SetContent(container.NewPadded(copyBtn))
 
-	// Create a visible main window
-	mainWindow := myApp.NewWindow("Click Catcher")
-	mainWindow.Resize(fyne.NewSize(200, 100))
-
-	clickCatcher := NewClickCatcher(copyWindow)
-	mainWindow.SetContent(clickCatcher)
-
+	// Monitor text selection
 	go func() {
 		for {
 			newSelection := getSelectedText()
@@ -95,7 +55,5 @@ func main() {
 		}
 	}()
 
-	mainWindow.Show()
-	fmt.Println("Starting main application loop...")
 	myApp.Run()
 }
